efb978267bccd769311c1912d3611e838c1c1912
AJ Desamparado <ajDesamparado13@gmail.com>
2021-07-29 16:27:44 +0800

    Update asset helper

--- src/helpers.js
+++ src/helpers.js
@@ -8,37 +8,26 @@ export const removeElement = (element) => {
     return parent.removeChild(element)
 }
 
+export const assets = {
+    install : (Vue,config={})=>{
+        Vue._assets = (path="",storage="") => {
+            let storage_path = Arr.getProperty(Vue._config, 'assets_url', "");
+            if(storage){
+                storage_path = Arr.getProperty(Vue._config,`storage.${storage}`, storage_path )
+            }
+            return Str.joinWith(storage_path, path,"/");
+        };
 
-export const appendQueryStringMark = (value) => {
-    let str = value.toString();
-    return str.charAt(0) === '?' ? str : "?" + str;
-};
-export const removeSlashPrefix = (value) =>{
-    let str = value.toString();
-    return str.charAt(0) === '/' ? str.slice(1,str.length) : str;
+        Object.defineProperties(Vue.prototype,{
+            $_assets:{
+                get:() => {
+                    return Vue._assets;
+                }
+            }
+        })
+    }
 }
-
-export const removeSlashSuffix = (value) =>{
-    let str = value.toString();
-    return str.charAt(str.length - 1) === '/' ? str.slice(0,str.length - 1) : str;
-}
-
-
-export const assets = (path="",storage="") => {
-    let storage_path = Arr.getProperty(Vue._config,`storage.${storage}` , Vue._config.assets_url ) || ""
-    return Str.joinWith(storage_path, path,"/");
-}
-
 
 export const VUE_INSTALLER = function( Vue ,config={}){
-
-    Vue._assets = assets;
-
-    Object.defineProperties(Vue.prototype,{
-        $_assets:{
-            get:() => {
-                return Vue._assets;
-            }
-        }
-    })
+    Vue.use(assets, Arr.getProperty(config, 'assets'));
 }
